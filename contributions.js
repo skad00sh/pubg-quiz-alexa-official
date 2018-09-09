4e9d8cb261c90641d0789803be37663e2f9f1179
Heramb <heramblonkar@gmail.com>
2018-09-09 17:40:22 +0530

    Added questions and contributors
    two questinos with help of Viren Soni and Ronit Sharma

---
+++ contributions.js
@@ -0,0 +1,19 @@
+//contributors
+
+
+//Viren Soni and Ronit Sharma
+
+      'Which is the lowest powered scope available in PUBG?'
+
+        '2x'
+        '1x'
+        '0x'
+        '4x'
+
+
+      'Which is the highest powered scope available in PUBG?'
+
+            '15x'
+            '20x'
+            '10x'
+            '8x'
---
+++ question.js
@@ -0,0 +1,228 @@
+
+        'What is the community name of two cement buildings containing high tier loot in Mirarmar?'
+
+            'God Building'
+            'Apartment'
+            'Shack'
+            'Warehouse'
+
+
+
+
+
+        'Which is the fastest vehicle in Miramar?'
+
+              'Mirado'
+              'Buggy'
+              'Pickup Truck'
+              'Van'
+
+
+
+
+        'In Pecado Miramar which buidling contains high tier loot?'
+
+              'Stadium or Boxing Ring'
+              'Apartments'
+              'Casino'
+              'Motel'
+
+
+
+     'Which non-drop Assault Rifle has highest damage?'
+
+           'AKM'
+           'M416'
+           'M16A4'
+           'SCAR-L'
+
+
+
+
+     'Which of the following gun has been removed from drop?'
+
+           'Tommy Gun'
+           'UMP'
+           'AKM'
+           'M416'
+
+
+
+
+     'Which is the non-drop LMG available in PUBG?'
+
+           'DP 28'
+           'Groza'
+           'M249'
+           'Mini 14'
+
+
+
+
+     'What is the type of ammunation AWM uses?'
+
+           '.300 Magnum'
+           '9mm'
+           '.45 ACP'
+           '12 Gauge'
+
+
+
+
+     'What is the name of only Designated Marksman Rifle which uses 5.56mm ammunation in MIRAMAR or ERANGLE?'
+
+           'Mini 14'
+           'SKS'
+           'SLR'
+           'VSS'
+
+
+
+
+     'What is the name of only Designated Marksman Rifle which uses 9mm ammunation?'
+
+           'VSS'
+           'SKS'
+           'MK 14'
+           'Mini 14 '
+
+
+
+
+     'Which is the most silent gun in Assault Rifle Designated Marksman Rifle and Sniper Rifle catagory?'
+
+           'VSS'
+           'SKS'
+           'MK 14'
+           'SLR'
+
+
+
+
+     'Which side arm has most damage?'
+
+           'Sawed off'
+           'P92'
+           'R45'
+           'R1895'
+
+
+
+
+     'The only Sniper Rifle which doesn\'t have any sight or scope slot is? '
+
+           'Win94'
+           'Kar98'
+           'AWM'
+           'M24'
+
+
+
+
+     'Some Pistols can use sight. What is the name of that sight?'
+
+           'Red Dot'
+           'Holographic'
+           '2x Scope'
+           'Pistols can\'t use any sight'
+
+
+
+
+     'Which muzzle attachment is the best for stability of weapon?'
+
+           'Compensator'
+           'Suppressor'
+           'Flash Hider'
+           'All attachments decrease the stability'
+
+
+
+
+     'Which stock attachment reduces sway in Sniper Rifle?'
+
+           'Cheek Pad'
+           'Tactical Stock'
+           'Bullet Loops'
+           'Vertical Foregrip'
+
+
+
+     'Which attachment increases aiming down sight speed?'
+
+           'Thumb Grip'
+           'Angled Grip'
+           'Lightweight Grip'
+           'Vertical Foregrip'
+
+
+
+     'Which Sub Machine Gun has highest clip?'
+
+           'Tommy Gun'
+           'UMP'
+           'Micro UZI'
+           'Vector'
+
+
+
+     'What is most common request to PUBG developers?'
+
+           'Region Lock China'
+           'Region Lock America'
+           'Region Lock Europe'
+           'Region Lock Africa'
+
+
+
+     'What is most common request to PUBG developers?'
+
+           'Fix PUBG'
+           'Do not fix PUBG'
+           'Offiline Mode'
+           '1 v 1 Mode'
+
+
+
+     'Where PUBG invitational 2018 was held?'
+
+           'Berlin'
+           'New York'
+           'Shanghai'
+           'Pochinki'
+
+
+
+     'When did PUBG Invitational 2018 take place?'
+
+           '25th July'
+           '25th June'
+           '25th May'
+           '31st June'
+
+
+
+     'What does sound louder while walking on the wooden surface of the following?'
+
+           'Shoes'
+           'Bare Foot'
+           'Bare Foot and Shoes both sounds same'
+           'There are no sounds at all'
+
+
+
+     'Who of the following is considered to be one of the best PUBG streamer?'
+
+           'Shroud'
+           'Arteezy'
+           'Simple'
+           'Ninja'
+
+
+
+     'PUBG is based on?'
+
+           'Arma 3'
+           'Dota 2'
+           'Overwatch'
+           'Need for Speed'
